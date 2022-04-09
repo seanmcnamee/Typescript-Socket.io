792defcb3c37609aee04111f8c1a6aba517d0924
Sean McNamee <36552436+seanmcnamee@users.noreply.github.com>
2022-04-09 14:04:38 -0400

    Update client.js

--- SkeletonClient/src/client.js
+++ SkeletonClient/src/client.js
@@ -1,6 +1,6 @@
 var socket = io.connect('http://seanmcnamee.ddns.net:3000');
 function sendMsg() {
-    socket.emit("boo nigga", "Bitch tits");
+    socket.emit("boo", "Bitch tits");
 }
 socket.on('boop', function (data) {
     console.log(data);
